f50000c1fa3d9ec8764d304fdb6571d6b3acae6e
webfoods <3497856794@qq.com>
2021-01-06 20:03:42 +0800

    Test

--- src/main/java/com/example/demo/controller/IndexContorller.java
+++ src/main/java/com/example/demo/controller/IndexContorller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexContorller {
     @RequestMapping("/index")
-    //ssadsd
+    //ssadsdsadsads
     public String index() {
 
         return "index";
